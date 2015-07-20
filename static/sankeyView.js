a35efc8b5bbd2db6780905edd002b7858e445440
psanker <psanker@splunk.com>
2015-07-20 15:27:43 -0700

    trying to get basic sankey working

--- static/sankeyView.js
+++ static/sankeyView.js
@@ -1,12 +1,15 @@
 $(document).ready(function() {
-	var nodes = [];
+	var nodes = [{name: "San Francisco"}];
 	var links = [];
+	var testNodes = [{name: "San Francisco"}];
 
-	$.ajax({
+	var activitiesDfD =  $.ajax({
 		url: "http://localhost:8080/district/1/activities",
 		type: "GET",
-		dataType: "json", 
-		success: function(data) {
+		dataType: "json"
+	});
+
+	$.when(activitiesDfD).then(function(data, textStatus, jqXhr) {
 			var total = 0;
 			var activityCodes = []; 
 			for(var i = 0; i < data.length; i++) {
@@ -14,39 +17,121 @@ $(document).ready(function() {
 				expenditure = parseFloat(expenditure);
 				total += expenditure;
 				nodes.push({name: data[i].Name});
+				testNodes.push({name: data[i].Name});
+				links.push({source: 0, target: i, value: expenditure});
 				activityCodes.push(data[i].Code);
 			}
 			var expenseCodes = [];
 
-			for (var i = 0; i < activityCodes.length; i++) {
+			var subActivitiesDfds = [];
+			var subActivitiesCodes = [];
 
-				$.ajax({
+			for (var i = 0; i < activityCodes.length; i++) {
+				subActivitiesDfds.push( $.ajax({
 					url: "http://localhost:8080/district/1/activities/" + activityCodes[i] + "/subActivities",
 					type: "GET",
 					dataType: "json",
 					success: function(data) {
-						for (var k = 0; k < data.length; k++) {
-							nodes.push({name: data[i].Name});
-							$.ajax({
-								url: "http://localhost:8080/district/1/subActivities/" + data[i].Code + "/expenses",
-								type: "GET",
-								dataType: "json"
-							})							
+						if (data) {
+							for (var i = 0; i < data.length; i++) {
+								nodes.push({name: data[i].Name});
+								subActivitiesCodes.push(data[i].Code);
+							}
 						}
-
-					},
-
-					error: function(xhr, status, errorThrown) {
-						console.log(status);
 					}
-
-
-				})
+				}));
 			}
-		},
 
-		error: function(xhr, status, errorThrown) {
-			console.log(status);
-		}
+			$.when.apply($, subActivitiesDfds).done(function(data, textStatus, jqXhr){
+				var expensesDfds = [];
+				for (var k = 0; k < subActivitiesCodes.length; k++) {
+					expensesDfds.push($.ajax({
+						url: "http://localhost:8080/district/1/subActivities/" + subActivitiesCodes[i].Code + "/expenses",
+						type: "GET",
+						dataType: "json",
+						success: function(data) {
+							if (data) {
+								for (var i = 0; i < data.length; i++) {
+									nodes.push({name: data[i].Name});
+								}
+							}
+						}
+					}));			
+				}
+				$.when.apply($, expensesDfds).then(function(data, textStatus, jqXhr){
+					console.log(nodes);
+					var svg = d3.select("#chart").append("svg").append("g");
+					var color = d3.scale.category20();
+					var sankey = d3.sankey()
+						.nodeWidth(30)
+						.nodePadding(10)
+						.size([500, 500])
+						.nodes(testNodes)
+						.links(links)
+						.layout(32);
+					var path = sankey.link();
+					var linkSvg = svg.append("g").selectAll(".link")
+						.data(links)
+						.enter()
+						.append("path")
+						.attr({
+							"class": "link",
+							d: path
+						})
+						.style("stroke-width", function(d) {
+							return Math.max(1, d.dy);
+						});
+
+					linkSvg.append("title")
+						.text(function(d) {
+							return d.source.name + " to " + d.target.name + " = " + d.value;
+						})
+					var nodes = svg.append("g").selectAll(".node")
+						.data(testNodes)
+						.enter()
+						.append("g")
+						.attr({
+							"class": "node",
+							transform: function(d) {
+								return "translate(" + d.x + "," + d.y + ")";
+							}
+						});
+					nodes.append("rect")
+						.attr({
+							height: function(d) {
+								return d.dy;
+							},
+							width: sankey.nodeWidth()
+						})
+						.style({
+							fill: function(d) {
+				            	return d.color = color(d.name.replace(/ .*/, ""));
+							},
+							stroke: function(d) {
+								return d3.rgb(d.color).darker(2);
+							}
+						})
+						.append("title")
+						.text(function(d) {
+							return d.name;
+						});
+
+					nodes.append("text")
+						.attr({
+							x: sankey.nodeWidth()/2,
+							y: function(d) {
+								return d.dy/2;
+							},
+							dy: ".35em",
+							"text-anchor": "middle",
+							transform: null
+						})
+						.text(function(d) {
+							return d.name;
+						});
+
+				});
+			});
+
 	});
 });
