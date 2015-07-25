1b06c96d1a84b3dd10bf0bfea2d25fbc53c68a88
psanker <psanker@splunk.com>
2015-07-24 17:41:42 -0700

    got three levels of icicle, doesn't look good

--- static/tree.js
+++ static/tree.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-	var root = {name: "San Francisco Budget", children: []};
+	var root = {level: 1, name: "San Francisco Budget", children: []};
 	var activitiesDfD =  $.ajax({
 		url: "http://localhost:8080/district/1/activities",
 		type: "GET",
@@ -8,50 +8,95 @@ $(document).ready(function() {
 
 	$.when(activitiesDfD).then(function(data, textStatus, jqXhr) {
 		var total = 0;
+		var activityCodes = [];
+		var expenseCodes = [];
+		var subActivitiesDfds = [];
+		var subActivitiesCodes = [];
+		var expenseDfds = [];
+
+		var addExpenses = function(parent) {
+			return function(data) {
+				if (data) {
+					for (var i = 0; i < data.length; i++) {
+						var expenditure = data[i].RestrictedExpenditure;
+						expenditure = parseFloat(expenditure);
+						var child = {level: 4, name: data[i].Name, size: expenditure, children: []};
+						parent.children.push(child);
+					}
+				}
+			}
+		}
+		var addSubActivities = function(parent) {
+			return function(data) {
+				if (data) {
+					for (var i = 0; i < data.length; i++) {
+						var expenditure = data[i].Expenditure;
+						expenditure = parseFloat(expenditure);
+						var child = {level: 3, name: data[i].Name, size: expenditure, children: []};
+						parent.children.push(child);
+						var dfd = $.getJSON("http://localhost:8080/district/1/subActivities/" + data[i].Code + "/expenses", addExpenses(child));
+						subActivitiesDfds.push(dfd);
+					}
+				}
+			}
+		}
+
 		for (var i = 0; i < data.length; i++) {
 			var expenditure = data[i].Expenditure;
 			expenditure = parseFloat(expenditure);
 			total += expenditure;
-			root.children.push({name: data[i].Name, size: expenditure});		
+			var child = {level: 2, name: data[i].Name, size: expenditure, children: []};
+			root.children.push(child);
+
+			var dfd = $.getJSON("http://localhost:8080/district/1/activities/" + data[i].Code + "/subActivities", addSubActivities(child));
+			subActivitiesDfds.push(dfd);
 		}
 		root.size = total;
+
+
+		$.when.apply($, subActivitiesDfds).done(function(data, textStatus, jqXhr) {
+			var width = 960,
+				height = 500;
+
+			var x = d3.scale.linear()
+				.range([0, width - 200]);
+			var y = d3.scale.linear()
+				.range([0, height]);
+			var color = d3.scale.category20();
+
+			var partition = d3.layout.partition()
+				.size([width, height])
+				.value(function(d) { return d.size;});
+
+			var nodes = partition.nodes(root);
+
+
+			var svg = d3.select("#chart").append("svg")
+				.attr("width", width)
+				.attr("height", height);
+
+			svg.selectAll(".node")
+				.data(nodes)
+				.enter().append("rect")
+				.attr("class", "node")
+			    .attr("x", function(d) { return d.x; })
+			    .attr("y", function(d) { return d.y; })
+			    .attr("width", function(d) { return d.dx; })
+			    .attr("height", function(d) { return d.dy; })
+			    .style("fill", function(d) { 
+			    	var test = (d.children ? d : d.parent).name;
+			    	console.log(test);
+			    	console.log(color(test));
+			    	console.log('====');
+			    	return color(d.level); 
+			    });
+		});
+
+
+
 		// debugger
 
-		var width = 960,
-			height = 500;
 
-		var x = d3.scale.linear()
-			.range([0, height]);
-		var y = d3.scale.linear()
-			.range([0, height]);
-		var color = d3.scale.category20();
-
-		var partition = d3.layout.partition()
-			.size([width, height])
-			.value(function(d) { return d.size;});
-
-		var nodes = partition.nodes(root);
-
-
-		var svg = d3.select("#chart").append("svg")
-			.attr("width", width)
-			.attr("height", height);
-
-		svg.selectAll(".node")
-			.data(nodes)
-			.enter().append("rect")
-			.attr("class", "node")
-		    .attr("x", function(d) { return d.x; })
-		    .attr("y", function(d) { return d.y; })
-		    .attr("width", function(d) { return d.dx; })
-		    .attr("height", function(d) { return d.dy; })
-		    .style("fill", function(d) { 
-		    	var test = (d.children ? d : d.parent).name;
-		    	console.log(test);
-		    	console.log(color(test));
-		    	console.log('====');
-		    	return color((d.children ? d : d.parent).name); 
-		    });
 
 
 		// var rect = svg.selectAll("rect");
