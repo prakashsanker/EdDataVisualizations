30a6048751d150990ef6b3d147cf65bf3f56b91a
psanker <praks5432@gmail.com>
2015-09-13 15:54:13 -0700

    attempt to write async reducers

--- scripts/actions.js
+++ scripts/actions.js
@@ -21,3 +21,28 @@ export function receiveDistricts(geoState, json) {
 		districts: json.data.districts.map(district => district.data)
 	}
 }
+
+export function fetchDistricts(geoState) {
+
+	return function (dispatch) {
+		dispatch(requestDistricts(geoState));
+		var districtsDfd = $.ajax({
+			url: "http://localhost:8080/districts/",
+			type: "GET",
+			dataType: "json"
+		});
+		var demographiesDfd = [];
+		$.when(districtsDfd).then(function(data, textStatus, jqXhr){
+			if (data) {
+				_.each(data, datum => 
+					var id = datum.id;
+					demographiesDfd.push($.getJSON("http://localhost:8080/district/${id}/demography"));
+				);
+			}
+		});
+		$.when(...demographiesDfd).done(result =>
+			//so I have demographic data right now. 
+			dispatch(receiveDistricts(geoState, result));
+		);
+	}
+}
